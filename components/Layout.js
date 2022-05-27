c974ed6373fe51a27a8f12a69e83948d17c3fbe4
Breezy Fasano <breezyfasano@gmail.com>
2022-05-27 15:00:34 -0500

    Adjust fonts

--- components/Layout.js
+++ components/Layout.js
@@ -2,10 +2,16 @@ import React from 'react'
 import Navbar from './Navbar'
 import AnnouncementBar from './AnnouncementBar'
 import Footer from './Footer'
+import Head from 'next/head'
 
 export default function Layout({children}) {
   return (
     <>
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@500;600;900&display=swap" rel="stylesheet"/>
+    </Head>
     <AnnouncementBar />
     {/* <Navbar /> */}
     <main className="container-fluid m-0 p-0">
