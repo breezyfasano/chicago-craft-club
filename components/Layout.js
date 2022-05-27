5e8d4acab61433608757452011ab0e555d2336da
Breezy Fasano <breezyfasano@gmail.com>
2022-05-27 15:03:43 -0500

    Fix crossorigin

--- components/Layout.js
+++ components/Layout.js
@@ -9,7 +9,7 @@ export default function Layout({children}) {
     <>
     <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
       <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@500;600;900&display=swap" rel="stylesheet"/>
     </Head>
     <AnnouncementBar />
