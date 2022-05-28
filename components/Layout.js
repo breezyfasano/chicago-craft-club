dde7c1f67ab56a7a95f183039ae45302d174c3bb
Breezy Fasano <breezyfasano@gmail.com>
2022-05-28 17:10:38 -0500

    SEO info

--- components/Layout.js
+++ components/Layout.js
@@ -4,20 +4,24 @@ import AnnouncementBar from './AnnouncementBar'
 import Footer from './Footer'
 import Head from 'next/head'
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
   return (
     <>
-    <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-      <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@500;600;900&display=swap" rel="stylesheet"/>
-    </Head>
-    <AnnouncementBar />
-    {/* <Navbar /> */}
-    <main className="container-fluid m-0 p-0">
-      {children}
-    </main>
-    <Footer />
+      <Head>
+        <title>Chicago Craft Club</title>
+        <meta name="description" content="A group for anyone in Chicago (or around Chicago) who is interested in crafting!" />
+        <meta itemprop="name" content="Chicago Craft Club" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@500;600;900&display=swap" rel="stylesheet" />
+
+      </Head>
+      <AnnouncementBar />
+      {/* <Navbar /> */}
+      <main className="container-fluid m-0 p-0">
+        {children}
+      </main>
+      <Footer />
     </>
   )
 }
