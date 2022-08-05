e842564898f5269ec3f69b8c90089d31cb219171
Breezy Fasano <breezyfasano@gmail.com>
2022-08-05 10:53:04 -0500

    Fix links to vendor websites and update announcement bar

--- components/AnnouncementBar.js
+++ components/AnnouncementBar.js
@@ -1,19 +1,16 @@
-import React from 'react'
-
+import Link from 'next/link'
 export default function AnnouncementBar() {
   return (
     <div className='bg-yellow text-center border-black border-solid border-t-4 border-b-4 px-0 py-2'>
       <span className='font-black text-md mb-0'>
         The Bans Off Our Bodies craft fair for reproductive rights is on August
         12!{' '}
-        <a
-          href='https://www.instagram.com/p/Cfwfdl_uDQl/'
-          target='_blank'
-          rel='noreferrer'
-        >
-          {' '}
-          More info.
-        </a>
+        <Link href='/bans-off-our-bodies'>
+          <a target='_blank' rel='noreferrer'>
+            {' '}
+            More info.
+          </a>
+        </Link>
       </span>
     </div>
   )
