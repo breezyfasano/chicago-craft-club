aa83e48adfce024bc7e18e51e5ecf4b747c8e723
Breezy Fasano <breezy.fasano@dbtlabs.com>
2024-09-04 21:26:01 -0500

    Adjust meta

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,18 +5,22 @@ import Head from 'next/head'
 type LayoutProps = {
   children: React.ReactNode
   pageClass?: string
+  metaTitle?: string
+  metaDescription?: string
 }
 
-export default function Layout({ children, pageClass }: LayoutProps) {
+export default function Layout({
+  children,
+  metaTitle = 'Chicago Craft Club',
+  metaDescription = 'A group for anyone in Chicago (or around Chicago) who is interested in crafting!',
+  pageClass,
+}: LayoutProps) {
   return (
     <>
       <Head>
-        <title>Chicago Craft Club</title>
-        <meta
-          name='description'
-          content='A group for anyone in Chicago (or around Chicago) who is interested in crafting!'
-        />
-        <meta itemProp='name' content='Chicago Craft Club' />
+        <title>{metaTitle}</title>
+        <meta name='description' content={metaDescription} />
+        <meta itemProp='name' content={`${metaTitle}`} />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       </Head>
       <Navbar />
