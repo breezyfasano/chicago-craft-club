a1bd1d3e074e452782f22d1aec1debed082e6c37
Breezy <breezyfasano@gmail.com>
2024-12-26 15:56:08 -0600

    fuck you javascript date constructor

--- components/Cards/EventCard.tsx
+++ components/Cards/EventCard.tsx
@@ -5,17 +5,11 @@ type EventCardProps = {
 }
 
 export default function EventCard({ event }: EventCardProps) {
-  const prepareDate = new Date(event.date).toLocaleDateString('en-us', {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-  })
   return (
     <article className='bg-white rounded-md border-black border-2 shadow-lg p-6 flex flex-col justify-between'>
       <h2 className='font-black text-blue'>{event.title}</h2>
       <div className='eventInfo my-3'>
-        <p className='font-bold text-blue '>{prepareDate}</p>
+        <p className='font-bold text-blue '>{event.date}</p>
         <p className='font-bold '>{event.time}</p>
         <p>{event.description}</p>
       </div>
