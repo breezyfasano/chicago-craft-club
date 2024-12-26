6d56d57a1f6bf8e3ff7419ccd3fbe2624cffeb87
Breezy <breezyfasano@gmail.com>
2024-12-26 15:43:05 -0600

    Update event bugs

--- components/Cards/EventCard.tsx
+++ components/Cards/EventCard.tsx
@@ -11,17 +11,12 @@ export default function EventCard({ event }: EventCardProps) {
     month: 'short',
     day: 'numeric',
   })
-
-  const prepareTime = Intl.DateTimeFormat('en', {
-    hour: 'numeric',
-    minute: 'numeric',
-  }).format(new Date(event.date))
   return (
     <article className='bg-white rounded-md border-black border-2 shadow-lg p-6 flex flex-col justify-between'>
       <h2 className='font-black text-blue'>{event.title}</h2>
       <div className='eventInfo my-3'>
         <p className='font-bold text-blue '>{prepareDate}</p>
-        <p className='font-bold '>{prepareTime}</p>
+        <p className='font-bold '>{event.time}</p>
         <p>{event.description}</p>
       </div>
       <div className='text-center'>
